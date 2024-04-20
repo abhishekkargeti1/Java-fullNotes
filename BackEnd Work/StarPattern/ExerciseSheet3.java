09c4fc46dccc43f34483f1ed372e1d221c583ae0
Abhishek Kargeti1 <abhishek.kargeti1@gmail.com>
2024-04-20 17:58:12 +0530

    StarPatternExercise added

--- BackEnd Work/StarPattern/ExerciseSheet3.java
+++ BackEnd Work/StarPattern/ExerciseSheet3.java
@@ -75,22 +75,22 @@ class ExerciseSheet3{
         //     System.out.println("");
         // }
 
-            int k=0;
-        for(int i =1;i<=9;i++){
-            if(i<=5){
-                k++;
-            }else{
-                k--;
-            }
-            for(int j=1;j<=9;j++){
-                if(j>=6-k && j<=4+k){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+        //     int k=0;
+        // for(int i =1;i<=9;i++){
+        //     if(i<=5){
+        //         k++;
+        //     }else{
+        //         k--;
+        //     }
+        //     for(int j=1;j<=9;j++){
+        //         if(j>=6-k && j<=4+k){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
 
 
 
