29ad4f871cf52050e369fd6184b5b84b70ee3fd9
Abhishek Kargeti1 <abhishek.kargeti1@gmail.com>
2024-04-21 17:13:34 +0530

    Updated Notes

--- BackEnd Work/Loops And Exercise sheet/ExerciseSheet2.java
+++ BackEnd Work/Loops And Exercise sheet/ExerciseSheet2.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 public class ExerciseSheet2 {
     public static void main(String []args){
         // Write a program in Python/Java to display the first 10 natural numbers.
